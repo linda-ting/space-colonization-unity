64f260777e8b0fac2f9ce1a14f8d96e147752efe
Linda Ting <lstting@seas.upenn.edu>
2021-12-02 23:37:05 -0500

    change ui input to sliders

--- Assets/Scripts/UserInterfaceController.cs
+++ Assets/Scripts/UserInterfaceController.cs
@@ -11,34 +11,34 @@ namespace AssemblyCSharp.Assets.Scripts
         private TreeGenerator _treeGenerator;
 
         [SerializeField]
-        private InputField _growthLengthInput;
+        private Slider _growthLengthInput;
 
         [SerializeField]
-        private InputField _killDistanceInput;
+        private Slider _killDistanceInput;
 
         [SerializeField]
-        private InputField _perceptionDistanceInput;
+        private Slider _perceptionDistanceInput;
 
         [SerializeField]
-        private InputField _perceptionRadiusInput;
+        private Slider _perceptionRadiusInput;
 
         [SerializeField]
-        private InputField _randomGrowthInput;
+        private Slider _randomGrowthInput;
 
         [SerializeField]
-        private InputField _trunkDiameterInput;
+        private Slider _trunkDiameterInput;
 
         [SerializeField]
-        private InputField _diameterCoeffInput;
+        private Slider _diameterCoeffInput;
 
         [SerializeField]
-        private InputField _treeAgeInput;
+        private Slider _treeAgeInput;
 
         [SerializeField]
-        private InputField _branchingAngleInput;
+        private Slider _branchingAngleInput;
 
         [SerializeField]
-        private InputField _numBranchesInput;
+        private Slider _numBranchesInput;
 
         private bool _isPaused = true;
 
@@ -47,58 +47,17 @@ namespace AssemblyCSharp.Assets.Scripts
         {
             Debug.Log("updating params!");
 
-            if (!string.IsNullOrEmpty(_growthLengthInput.text))
-            {
-                Branch.GrowthLength = float.Parse(_growthLengthInput.text.Trim());
-            }
+            Branch.GrowthLength = _growthLengthInput.value;
+            Branch.KillDistance = _killDistanceInput.value;
+            Branch.PerceptionLength = _perceptionDistanceInput.value;
+            Branch.PerceptionRadius = _perceptionRadiusInput.value;
+            Branch.RandomGrowthParam = _randomGrowthInput.value;
+            Branch.TrunkDiameter = _trunkDiameterInput.value;
+            Branch.DiameterCoeff = _diameterCoeffInput.value;
+            Branch.BranchingAngle = _branchingAngleInput.value;
+            Branch.MaxBranching = (int)_numBranchesInput.value;
 
-            if (!string.IsNullOrEmpty(_killDistanceInput.text))
-            {
-                Branch.KillDistance = float.Parse(_killDistanceInput.text.Trim());
-            }
-
-            if (!string.IsNullOrEmpty(_perceptionDistanceInput.text))
-            {
-                Branch.PerceptionLength = float.Parse(_perceptionDistanceInput.text.Trim());
-            }
-
-            if (!string.IsNullOrEmpty(_perceptionRadiusInput.text))
-            {
-                Branch.PerceptionRadius = float.Parse(_perceptionRadiusInput.text.Trim());
-            }
-
-            if (!string.IsNullOrEmpty(_randomGrowthInput.text))
-            {
-                Branch.RandomGrowthParam = float.Parse(_randomGrowthInput.text.Trim());
-            }
-
-            if (!string.IsNullOrEmpty(_trunkDiameterInput.text))
-            {
-                Branch.TrunkDiameter = float.Parse(_trunkDiameterInput.text.Trim());
-            }
-
-            if (!string.IsNullOrEmpty(_diameterCoeffInput.text))
-            {
-                Branch.DiameterCoeff = float.Parse(_diameterCoeffInput.text.Trim());
-            }
-
-            if (!string.IsNullOrEmpty(_treeAgeInput.text))
-            {
-                int age = int.Parse(_treeAgeInput.text.Trim());
-                _treeGenerator.SetTreeAge(age);
-            }
-
-            if (!string.IsNullOrEmpty(_branchingAngleInput.text))
-            {
-                int angle = int.Parse(_branchingAngleInput.text.Trim());
-                Branch.BranchingAngle = angle;
-            }
-
-            if (!string.IsNullOrEmpty(_numBranchesInput.text))
-            {
-                int num = int.Parse(_numBranchesInput.text.Trim());
-                Branch.MaxBranching = num;
-            }
+            _treeGenerator.SetTreeAge((int)_treeAgeInput.value);
         }
 
         public void ResetParamsCallback()
